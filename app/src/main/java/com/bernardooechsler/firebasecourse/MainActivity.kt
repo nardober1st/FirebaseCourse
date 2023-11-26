ccc2864a74ea5110804ef1646a0c9219b926e131
nardober1st <bernardo.oechsler6@gmail.com>
2023-11-26 02:25:03 -0800

    unnecessary comments deleted

--- app/src/main/java/com/bernardooechsler/firebasecourse/MainActivity.kt
+++ app/src/main/java/com/bernardooechsler/firebasecourse/MainActivity.kt
@@ -3,20 +3,10 @@ package com.bernardooechsler.firebasecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.bernardooechsler.firebasecourse.presentation.navigation.Navigation
 import com.bernardooechsler.firebasecourse.presentation.navigation.rootnavgraph.RootNavigationGraph
 import com.bernardooechsler.firebasecourse.ui.theme.FirebaseCourseTheme
-import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
